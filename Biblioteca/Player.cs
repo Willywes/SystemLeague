c9a4abbea585a3ca52f6ee8cde1ade7914274b70
Willywes <willywes_69@hotmail.com>
2016-09-16 16:25:42 -0300

    Actualizando Clases

---
+++ Biblioteca/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Player : Class
+    {
+
+        public string UserName { get; set; }
+        public string Pass { get; set; }
+        public Team Team { get; set; } // solo uno
+        
+
+        public Player(int id, string userName, string pass, string nombre, Team team, DateTime fechaCreacion) : base(id, nombre, fechaCreacion)
+        {
+            this.UserName = userName;
+            this.Pass = pass;
+            this.Team = Team;
+        }
+
+    }
+}
---
+++ Biblioteca/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Team : Class
+    {
+        public Team(int id, string nombre, DateTime fechaCreacion) : base(id, nombre, fechaCreacion)
+        {
+        }
+
+        private int IdAdmin { get; set; }
+    }
+}
